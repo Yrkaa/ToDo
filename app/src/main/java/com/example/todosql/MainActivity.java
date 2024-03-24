050cf81a7ecb9f7cadd60e8e564c3279e9a36630
Yrka <varnack30@gmail.com>
2024-03-24 17:50:10 +0300

    Сделал базовый код

---
+++ app/src/main/java/com/example/todosql/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.todosql;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Список с делами для адаптера под список
+    ArrayList<String> adapterList = new ArrayList<>();
+
+    //База данных
+    SQLiteDatabase db;
+
+    //Создание переменных для элементов разметки
+    RecyclerView doList;
+    Button addDo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Инициализация базы данных
+        db = getBaseContext().openOrCreateDatabase("doData.db", MODE_PRIVATE, null);
+
+        //Инициализация переменных для элементов разметки
+        doList = findViewById(R.id.do_list_rv);
+        addDo = findViewById(R.id.add_do_btn);
+    }
+}
