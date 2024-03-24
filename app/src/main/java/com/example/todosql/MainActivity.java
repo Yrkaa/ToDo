9958ebfecab925ca0429df64c410121bb3966edd
Yrka <varnack30@gmail.com>
2024-03-24 21:26:26 +0300

    Сделал получение уже существующих данных из базы данных

--- app/src/main/java/com/example/todosql/MainActivity.java
+++ app/src/main/java/com/example/todosql/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.todosql;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.Button;
@@ -29,9 +30,18 @@ public class MainActivity extends AppCompatActivity {
         //Инициализация базы данных
         db = getBaseContext().openOrCreateDatabase("doData.db", MODE_PRIVATE, null);
 
+        //Создание таблицы для дел, если она не существует
+        db.execSQL("CREATE TABLE IF NOT EXISTS Do (_id INTEGER PRIMARY KEY AUTOINCREMENT, txt TEXT NOT NULL)");
+
         //Инициализация переменных для элементов разметки
         doList = findViewById(R.id.do_list_rv);
         addDo = findViewById(R.id.add_do_btn);
+
+        //Получение существующих дел из бд
+        Cursor cursor = db.rawQuery("SELECT * FROM Do", null);
+        while (cursor.moveToNext()){
+            adapterList.add(cursor.getString(1));
+        }
 
         //Заполнение списка
         doList.setAdapter(new DoAdapter(adapterList, this));
